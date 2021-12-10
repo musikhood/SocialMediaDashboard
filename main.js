9e6b7791166d303f6c4796a3acadc9d016e95684
Rafał Białowski <b.bialowski@gmail.com>
2021-12-10 16:35:34 +0100

    Change slider

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ document.querySelector('.switch-checkbox').addEventListener("click",()=>{
         document.querySelector('html').style.setProperty('--bgtile','hsl(227,47%,96%)');
         document.querySelector('html').style.setProperty('--graytext','hsl(228, 12%, 44%)');
         document.querySelector('html').style.setProperty('--text','hsl(230,17%,14%)');
+        document.querySelector('html').style.setProperty('--hover','hsl(227,47%,90%)');
         dark = false;
         
     }
@@ -15,6 +16,8 @@ document.querySelector('.switch-checkbox').addEventListener("click",()=>{
         document.querySelector('html').style.setProperty('--bgtile','hsl(228,28%,20%)');
         document.querySelector('html').style.setProperty('--graytext','hsl(228,34%,66%)');
         document.querySelector('html').style.setProperty('--text','hsl(0,0%,100%)');
+        document.querySelector('html').style.setProperty('--hover','hsl(228,28%,25%)');
         dark = true;
     }
+    document.querySelector('.slider').classList.toggle('active');
 })
